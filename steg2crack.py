f99cc0be5eef463d7ef902638d070c97ec421886
fixploit03 <164528883+fixploit03@users.noreply.github.com>
2024-10-06 00:17:48 +0700

    Update steg2crack.py

--- steg2crack.py
+++ steg2crack.py
@@ -9,14 +9,14 @@ import subprocess
 file_path = input("Enter the Steghide file path: ")
 wordlist_path = input("Enter the wordlist file path: ")
 
-  with open(wordlist_path, "r", encoding="latin-1", errors="ignore") as f:
-      for line in f:
-          password = line.strip()  
-          command = f"steghide extract -sf {file_path} -p {password} -f"
-          result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-          if result.returncode == 0:
-              print(f"Password found: {password}") 
-              break
-          else:
-              print(f"Incorrcet password: {password}")
-print("Password not found, try a different wordlist.")
+with open(wordlist_path, "r", encoding="latin-1", errors="ignore") as f:
+    for line in f:
+        password = line.strip()  
+        command = f"steghide extract -sf {file_path} -p {password} -f"
+        result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        if result.returncode == 0:
+            print(f"Password found: {password}") 
+            break
+        else:
+            print(f"Incorrcet password: {password}")
+    print("Password not found, try a different wordlist.")
