0e1e18aa058c0d481354ec7a45979c81b614e6fd
fixploit03 <164528883+fixploit03@users.noreply.github.com>
2024-10-06 10:31:04 +0700

    Update steg2crack.py

--- steg2crack.py
+++ steg2crack.py
@@ -90,7 +90,7 @@ with open(file_wordlist, "r", encoding="latin-1", errors="ignore") as fw:
             print(f"{p}[{m}-{p}] Program dihentikan oleh pengguna.{r}")
             exit(1)
             
-if not kata_sandi_ditemukan:
-    waktu_akhir = datetime.now()
-    print(f"{p}[{m}-{p}] Kata sandi tidak ditemukan, coba file Wordlist yang lain.{r}")
-    print(f"\n{p}[{b}*{p}] Berakhir pada : {b}{waktu_akhir.strftime('%d-%m-%Y %H:%M:%S')}{r}")
+    if not kata_sandi_ditemukan:
+        waktu_akhir = datetime.now()
+        print(f"{p}[{m}-{p}] Kata sandi tidak ditemukan, coba file Wordlist yang lain.{r}")
+        print(f"\n{p}[{b}*{p}] Berakhir pada : {b}{waktu_akhir.strftime('%d-%m-%Y %H:%M:%S')}{r}")
