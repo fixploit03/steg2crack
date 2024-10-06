1a3a8473ba75f42df0b7807636332b875c634535
fixploit03 <164528883+fixploit03@users.noreply.github.com>
2024-10-06 14:06:33 +0700

    Update steg2crack.py

--- steg2crack.py
+++ steg2crack.py
@@ -56,6 +56,10 @@ if hasil_cek.returncode == 0:
     input(f"\n{p}Tekan [{h}Enter{p}] untuk memulai proses melanjutkan...{r}")
     os.system("clear")
 else:
+    # Cek sistem operasi 
+    cek_sistem_operasi = f"uname -o"
+    hasil_cek_sistem_operasi = subprocess.run(cek_sistem_operasi, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    print(hasil_cek_sistem_operasi)
     print(f"{p}[{m}-{p}] steghide belum terinstal.{r}")
     print(f"{p}[{m}-{p}] Anda dapat menginstalnya dengan mengetikkan perintah 'sudo apt-get install steghide'.{r}")
     exit(1)
