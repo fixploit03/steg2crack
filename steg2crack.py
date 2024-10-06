8ec4063f0ae5c7fc0b3c0313f68e354d7fc7005c
fixploit03 <164528883+fixploit03@users.noreply.github.com>
2024-10-06 14:19:39 +0700

    Update steg2crack.py

--- steg2crack.py
+++ steg2crack.py
@@ -62,8 +62,10 @@ else:
     hasil_cek_sistem_operasi = subprocess.run(cek_sistem_operasi, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     if hasil_cek_sistem_operasi.returncode == 0:
         sistem_operasi = hasil_cek_sistem_operasi.stdout.decode().strip()
+        # Android (Termux)
         if sistem_operasi == "Android":
             print(f"{p}[{m}-{p}] Anda dapat menginstalnya dengan mengetikkan perintah 'pkg install steghide'.{r}")
+        # Linux 
         elif sistem_operasi == "GNU/Linux":
             print(f"{p}[{m}-{p}] Anda dapat menginstalnya dengan mengetikkan perintah 'sudo apt-get install steghide'.{r}")
     exit(1)
