89156642b45a32d8212e5f752dee1abd552b6522
fixploit03 <164528883+fixploit03@users.noreply.github.com>
2024-10-10 19:13:56 +0700

    Update steg2crack.py

--- steg2crack.py
+++ steg2crack.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 ## Program   : steg2crack
-## Deskripsi : Program python3 sederhana yang dirancang untuk meng-crack file Stego.
+## Deskripsi : Program Python sederhana yang dirancang untuk meng-crack file Steganografi dengan teknik Dictionary Attack.
 ## Pembuat   : fixploit03 
 ## Rilis     : 5-10-2024
 ## Github    : https://github.com/fixploit03/steg2crack/
@@ -53,7 +53,8 @@ if sistem_operasi == "Linux":
 # Windows
 elif sistem_operasi == "Windows":
     os.system("cls")
-    
+
+# Banner cek steghide 
 print(f"""
 {p}                         ,
 {p}  ,-.       _,---._ __  / l
@@ -95,6 +96,7 @@ else:
     print(f"{p}[{m}-{p}] steghide belum terinstal.{r}")
     exit(1)
 
+# Banner program 
 print(f"""
 {c}      _             ____                     _    
 {c}  ___| |_ ___  __ _|___ \ ___ _ __ __ _  ___| | __
@@ -102,13 +104,9 @@ print(f"""
 {c} \__ \ ||  __/ (_| |/ __/ (__| | | (_| | (__|   < 
 {c} |___/\__\___|\__, |_____\___|_|  \__,_|\___|_|\_|
 {c}              |___/                               
-{p}[{b}*{p}] Program   : {b}steg2crack
-{p}[{b}*{p}] Deskripsi : {b}Program python3 untuk meng-crack file Stego
-{p}[{b}*{p}] Pembuat   : {b}fixploit03 
-{p}[{b}*{p}] Github    : {b}https://github.com/fixploit03/steg2crack/
-{p}[{b}*{p}] Team      : {b}ArSec (Arjuna Security)
 {r}""")
 
+# Meminta nama file Steganografi dari pengguna
 while True:
     try:
         file_stego = input(f"{p}[{b}#{p}] Masukkan nama file Stego : ")
@@ -126,6 +124,7 @@ while True:
         print(f"\n{p}[{m}-{p}] Terjadi kesalahan: {e}.{r}")
         exit(1)
 
+# Meminta nama file Wordlist dari pengguna
 while True:
     try:
         file_wordlist = input(f"{p}[{b}#{p}] Masukkan nama file Wordlist : ")
@@ -143,6 +142,7 @@ while True:
         print(f"\n{p}[{m}-{p}] Terjadi kesalahan: {e}.{r}")
         exit(1)
 
+# Meminta nama folder untuk menyimpan hasil file yang berharga di-crack
 while True:
     try:
         folder = input(f"{p}[{b}#{p}] Masukkan nama folder untuk menyimpan hasil file yang berhasil di-crack : ")
@@ -164,6 +164,7 @@ print("")
 
 kata_sandi_ditemukan = False
 
+# Proses cracking file Steganografi
 try:
     with open(file_wordlist, "r", encoding="latin-1", errors="ignore") as fw:
         daftar_kata_sandi = fw.read().splitlines()
